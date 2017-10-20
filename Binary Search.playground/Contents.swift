603952ba85976cad40595860d13d47a64136222a
Henrique Morbin <morbin_@hotmail.com>
2017-10-20 09:34:03 -0200

    Adding BigONotation and BinarySearch

---
+++ Binary Search.playground/Contents.swift
@@ -0,0 +1,24 @@
+// https://github.com/raywenderlich/swift-algorithm-club/tree/master/Binary%20Search
+
+// O(log n)
+func binarySearch<T: Comparable>(_ a: [T], key: T, range: Range<Int>) -> Int? {
+    if range.lowerBound >= range.upperBound {
+        return nil
+    } else {
+        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+        
+        if a[midIndex] > key {
+            return binarySearch(a, key: key, range: range.lowerBound ..< midIndex)
+        } else if a[midIndex] < key {
+            return binarySearch(a, key: key, range: midIndex + 1 ..< range.upperBound)
+        } else {
+            return midIndex
+        }
+    }
+}
+
+let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+
+binarySearch(numbers, key: 43, range: 0 ..< numbers.count)
+
+// Is it a problem that the array must be sorted first? It depends. Keep in mind that sorting takes time -- the combination of binary search plus sorting may be slower than doing a simple linear search. Binary search shines in situations where you sort just once and then do many searches.
